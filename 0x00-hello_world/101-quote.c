249394c8042ac14be5280f417e4f6a823245a374
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-17 20:56:44 +0000

    committing 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: exit code
+ */
+int main(void)
+{
+	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, buf, (int)sizeof(buf));
+	return (1);
+}
