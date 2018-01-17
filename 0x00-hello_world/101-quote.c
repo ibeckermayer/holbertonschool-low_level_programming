cd3580f96e1cad23a946ea4555bf9245e522a840
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-17 21:01:03 +0000

    fix line length

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-"
+		"19\n";
 
 	write(2, buf, (int)sizeof(buf));
 	return (1);
