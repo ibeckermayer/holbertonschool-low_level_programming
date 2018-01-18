b4ba20039513ec24027282b346a86c6d1d3f7b3c
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-18 22:37:10 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-	int d0, d1, d2, a;
+	int d0, d1, d2, a, b;
 
 	d0 = '0';
 	d1 = '1';
 	d2 = '2';
 	a = 1;
+	b = 2;
 
-	while (d0 <= '7' && d1 <= '8' && d2 <= '9')
+	while (d0 <= '9') /*&& d1 <= '9' && d2 <= '9')*/
 	{
-		if (!(d0 == d1 && d0 == d2))
+		if (d0 < d1 && d1 < d2)
 		{
 			putchar(d0);
 			putchar(d1);
@@ -30,9 +31,16 @@ int main(void)
 
 		if (d2 == '9')
 		{
-			d2 = '0' + a++;
-			d1++;
-			d0++;
+			if (d1 == '9')
+			{
+				d1 = '0';
+				d0++;
+			}
+			else
+				d1++;
+
+			d2 = '0';
+
 		}
 		else
 			d2++;
