9354b69d8b83bf78c47df53d21e0e1fb487a976f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-18 01:05:21 +0000

    this makes no sense

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	char b[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, b, sizeof(b)-1);
+	
+	write(2, b, (unsigned long)sizeof(b)-1);
 	return (1);
 }
