0cbd9f6f0052f862e3d6f904aea7fd7d36740a2f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-18 00:55:32 +0000

    fixing ip git

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,6 @@ int main(void)
 {
 	char b[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-<<<<<<< HEAD
 	write(2, b, (int)(sizeof(b)-1));
-=======
-	write(2, b, sizeof(b)-1);
->>>>>>> 3e1fd6758a70412679b10db172811c088e1e2bdb
 	return (1);
 }
