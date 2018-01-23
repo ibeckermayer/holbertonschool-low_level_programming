c5be68b70a24d438dd140ba8ecdc0b71fbf5f23e
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-23 21:38:00 +0000

    corrected 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,15 +9,12 @@ int main(void)
 {
 	int sum, i;
 
-	sum = i = 0;
-
-	while (i * 5 < 1024)
-		sum += i++ * 5;
-
-	i = 0;
-
-	while (i * 3 < 1024)
-		sum += i++ * 3;
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
 
 	printf("%d\n", sum);
 	return (0);
