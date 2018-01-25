3d076a47ab8210c062c53039bec941b10c41c8ae
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-25 21:31:43 +0000

    fixed 101 for boundary conds

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,19 @@
 #include "holberton.h"
 
 /**
+ * _abs - absolute value function
+ * @n: character to be abs'd
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+	return (n);
+}
+
+/**
  * _pow - takes a to the power of b
  * @a: operand
  * @b: operand
@@ -10,6 +23,9 @@
 int _pow(int a, int b)
 {
 	int result = a;
+
+	if (b == 0)
+		return (1);
 
 	while (--b > 0)
 	{
@@ -23,24 +39,27 @@ int _pow(int a, int b)
 void print_number(int n)
 {
 	int places = 1;
-	int divisor, dig, initial_divisor;
+	int divisor, dig;
 
 	if (n < 0)
 		_putchar('-');
 
-	while (n / _pow(10, places))
-		places++;
-
-	divisor = _pow(10, places);
-
-	initial_divisor = divisor;
-	while (divisor)
+	if (n > -10 && n < 10)
+		_putchar(_abs(n) + '0');
+	else
 	{
-		dig = (n / divisor) % 10;
-		if (n < 0)
-			dig *= -1;
-		if (!(divisor == initial_divisor && dig == 0))
+		while (_abs(n / _pow(10, places)) >= 10)
+			places++;
+
+		divisor = _pow(10, places);
+
+		while (divisor)
+		{
+			dig = (n / divisor) % 10;
+			if (n < 0)
+				dig = _abs(dig);
 			_putchar(dig + '0');
-		divisor /= 10;
+			divisor /= 10;
+		}
 	}
 }
