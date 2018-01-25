0140dc589ab6744ff5c729c5663e89b973d47693
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-25 17:18:35 +0000

    2 3 and 4

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers and returns the result
+ * @n1: first operand
+ * @n2: second operand
+ *
+ * Return: the result of the addition of n1 and n2
+ */
+int mul(int n1, int n2)
+{
+	return (n1 * n2);
+}
