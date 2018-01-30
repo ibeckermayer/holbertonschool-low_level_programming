ecbac89477c725f7830d6733f4eb89cd78b06794
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-30 21:43:08 +0000

    0

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - makes value ptd to by n = to 98
+ * @n: pointer to integer to be changed
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
