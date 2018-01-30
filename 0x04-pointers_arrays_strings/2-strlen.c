cd405acc3e43e263dea629aedfe0d456236f15f2
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-30 21:50:57 +0000

    2

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+	return (length);
+}
