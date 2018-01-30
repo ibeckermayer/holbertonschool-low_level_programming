e031c07268ee17e77c3daf5b3a5eebae28c4bf74
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-30 23:45:48 +0000

    fixed boundary condition

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	if (n == 0)
-		return;
-	else
+	if (n > 0)
 	{
 		for (i = 0; i < n - 1; i++)
 			printf("%d, ", *(a + i));
 		printf("%d\n", *(a + (n - 1)));
 	}
+	else
+		return;
 }
