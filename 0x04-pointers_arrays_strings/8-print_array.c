fd8f8d05e5d17c661f061e4f44969b8a569344e2
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-01-30 23:57:20 +0000

    going back

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- *
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s++)
-		length++;
-	return (length);
-}
-
-/**
  * print_array - prints first n elements of array
  * @a: array to be printed
  * @n: number of elements to be printed
@@ -25,7 +10,7 @@ int _strlen(char *s)
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; i < n - 1; i++)
 		printf("%d, ", *(a + i));
