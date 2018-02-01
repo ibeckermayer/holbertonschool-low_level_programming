5e618cc4348a49fe76264b4b3b5bffca4fff6218
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-01 21:22:38 +0000

    3

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+	return (length);
+}
+
+/**
+ * _strcmp - compares the strings
+ * @s1: string
+ * @s2: string
+ *
+ * Return: difference between first non-equal char
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
