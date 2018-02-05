4bb00083c7b2bf735180efa66e78c748f10c33a9
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-05 21:34:36 +0000

    3

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#define NULL 0
+
+/**
+ * isin - checks if character c is in string s
+ * @c: character
+ * @s: string
+ *
+ * Return: 1 for true, 0 for false
+ */
+int isin(char c, char *s)
+{
+	while (*s)
+		if (c == *s++)
+			return (1);
+	return (0);
+}
+
+/**
+ * _strspn - find number of bytes in the initial segment of s which consist only
+ * of bytes from accept
+ * @s: string to be checked
+ * @accept: string to be matched to
+ *
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int ret = 0;
+	while (*s)
+		if (isin(*s++, accept))
+			ret++;
+		else
+			return ret;
+	return ret;
+}
