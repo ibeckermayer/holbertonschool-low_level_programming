8e7e6a2751130bd5f831a0c9590fe18859b76692
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-05 20:28:22 +0000

    1

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: area copied to
+ * @src: area copied
+ * @n: number of bytes
+ *
+ * Return: a pointer to the memory area dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
