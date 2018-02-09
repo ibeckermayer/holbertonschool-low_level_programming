650faa5b23295000a78584667c475f06ef85189f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-09 22:21:13 +0000

    1

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argv);
+	printf("%d\n", argc-1);
+	return (0);
+}
