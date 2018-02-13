fbe46232192f0a30a482a84346041d2147a98dab
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-13 23:59:48 +0000

    1

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+	return (length);
+}
+
+/**
  * _strdup - returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
  * @str: string
@@ -11,4 +26,17 @@
  */
 char *_strdup(char *str)
 {
+	int len, i;
+	char *copy;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = _strlen(str);
+	if ((copy = malloc(len + 1)) == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		copy[i] = str[i];
+	return (copy);
+
 }
