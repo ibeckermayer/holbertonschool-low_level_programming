c06b242573a487e5a61bad18d33cc292379892e9
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-14 02:28:45 +0000

    3, freeing old memory if there's a null part way through

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -27,7 +27,11 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
