46049642e1dd223275f0ae49a5b3b4f4bc308ebb
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-14 03:11:39 +0000

    5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -2,6 +2,62 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+	return (length);
+}
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string1
+ * @s2: string2
+ *
+ * returned pointer should point to a newly allocated space in memory
+ * which contains the contents of s1, followed by the contents of s2,
+ * and null terminated
+ *
+ * if NULL is passed, treat it as an empty string
+ *
+ * Return: pointer to concatenated string or NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i, offset;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	concat = malloc(len1 + len2 + 2);
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	offset = i;
+	for (i = 0; i < len2; i++)
+		concat[i + offset] = s2[i];
+	concat[i++ + offset] = '\n';
+	concat[i + offset] = '\0';
+
+	return (concat);
+}
+
+/**
  * argstostr - concatenates all the arguments of the program
  * @ac: argument count
  * @av: arguments
@@ -12,4 +68,18 @@
  */
 char *argstostr(int ac, char **av)
 {
+	char *ret_string = NULL;
+	int i;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+		ret_string = str_concat(ret_string, av[i]);
+	}
+
+	return ret_string;
 }
