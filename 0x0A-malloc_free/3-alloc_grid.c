2fd4457acab9eb1f9dda19681d72348e8dba2e7f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-14 00:51:46 +0000

    3, initializing at 0

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -13,22 +13,26 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **rows;
-	int i;
+	int **grid;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	rows = malloc(sizeof(int *) * height);
-	if (rows == NULL)
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		rows[i] = malloc(sizeof(int) * width);
-		if (rows[i] == NULL)
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 			return (NULL);
 	}
 
-	return (rows);
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+
+	return (grid);
 }
