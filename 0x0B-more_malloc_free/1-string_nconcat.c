f9f659718afc9f5f2683786d972e9e353d929887
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-15 21:39:42 +0000

    2

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+	return (length);
+}
+
+/**
+ * str_concat - concatenates s1 with the first n bytes of s2 and null terminated
+ * @s1: string1
+ * @s2: string2
+ *
+ * If n is greater or equal to the length of s2 then use the entire string s2
+ * if NULL is passed, treat it as an empty string
+ *
+ * Return: pointer to concatenated string or NULL on failure.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1, len2, i, offset;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (n < len2)
+		len2 = n;
+
+	concat = malloc(len1 + len2 + 1);
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	offset = i;
+	for (i = 0; i <= len2; i++)
+		concat[i + offset] = s2[i];
+
+	return (concat);
+}
