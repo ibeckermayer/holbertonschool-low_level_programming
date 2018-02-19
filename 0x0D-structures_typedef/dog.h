e764eab00d79af74660995c931f7b57c9dc87b50
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-19 21:04:13 +0000

    3

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -9,12 +9,12 @@ int _putchar(char c);
  *
  * Description: structure for a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
