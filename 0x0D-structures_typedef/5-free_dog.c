b0b4f1905234ce9d51efea3856b83085d120a3d0
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-19 23:25:25 +0000

    5

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -9,10 +9,7 @@
  */
 void free_dog(dog_t *d)
 {
-	dog_t *d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
-
-	init_dog(d, name, age, owner);
-	return d;
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
