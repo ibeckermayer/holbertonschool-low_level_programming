2cf8ae78becb0505e6a21c5477f8257645abc413
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-19 19:25:40 +0000

    2 and gitignore so it doesn't block it

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file, followed by a newline
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
