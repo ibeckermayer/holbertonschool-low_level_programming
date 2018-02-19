6b303085525def350be6b6e7c640b0d866f89827
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-19 20:23:14 +0000

    0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+int _putchar(char c);
+/**
+ * dog - a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: structure for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
