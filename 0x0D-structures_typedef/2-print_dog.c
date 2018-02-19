fcca77eb449f241679d9ac50da9771f57531461d
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-19 20:55:03 +0000

    2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,35 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
-
-/**
- * _puts - prints a string followed by a new line
- * @str: string
- *
- * Return: void
- */
-void _puts(char *str)
-{
-	while (*str)
-		_putchar(*str++);
-	_putchar('\n');
-}
-
-/**
- * f_to_str - converts float to a string
- */
-char *f_to_str(float f)
-{
-
-}
+#include <stdio.h>
 
 /**
  * print_dog - prints a dog
+ * @d: dog
  *
  * Return: void
  */
 void print_dog(struct dog *d)
 {
-	_puts(d->name);
-	_putchar(d->age + '0')
-	d->owner = owner;
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
