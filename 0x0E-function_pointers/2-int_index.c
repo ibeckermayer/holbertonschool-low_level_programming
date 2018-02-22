a12d80a7d9b9ca4bb6d457ed139ea3f1bc4d2631
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-22 02:43:13 +0000

    betty compliant

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -18,8 +18,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && cmp != NULL && size > 0)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
-			    return (i);
+				return (i);
 
-	return(FAILURE);
+	return (FAILURE);
 
 }
