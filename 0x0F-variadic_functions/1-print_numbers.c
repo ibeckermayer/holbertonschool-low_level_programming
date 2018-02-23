f20d2fb88195c56305780be31b65038e38c1190b
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-02-23 19:10:17 +0000

    1 and 2

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: is the string to be printed between numbers
+ * @n: number of params
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	va_start(valist, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d%s", va_arg(valist, int), separator);
+		printf("%d", va_arg(valist, int));
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+			printf("%d", va_arg(valist, int));
+	}
+	va_end(valist);
+
+	printf("\n");
+}
---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: is the string to be printed between the strings
+ * @n: number of params
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *curstr;
+
+	va_start(valist, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			curstr = va_arg(valist, char *);
+			if (curstr != NULL)
+				printf("%s%s", curstr, separator);
+			else
+				printf("%s%s", "(nil)", separator);
+		}
+		curstr = va_arg(valist, char *);
+		if (curstr != NULL)
+			printf("%s", curstr);
+		else
+			printf("%s", "(nil)");
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			curstr = va_arg(valist, char *);
+			if (curstr != NULL)
+				printf("%s", curstr);
+			else
+				printf("%s", "(nil)");
+		}
+	}
+	va_end(valist);
+	printf("\n");
+}
