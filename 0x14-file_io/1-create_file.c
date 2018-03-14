169027f6110601165a4efadde05bd299bc5e1262
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-03-14 01:58:34 +0000

    betty '

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -8,11 +8,11 @@
  */
 int _strlen(char *s)
 {
-        int length = 0;
+	int length = 0;
 
-        while (*s++)
-                length++;
-        return (length);
+	while (*s++)
+		length++;
+	return (length);
 }
 
 /**
@@ -24,12 +24,12 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-        int len = _strlen(src);
+	int len = _strlen(src);
 
-        while (*src)
-                *dest++ = *src++;
-        *dest = '\0';
-        return (dest - len);
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (dest - len);
 }
 
 /**
@@ -41,26 +41,26 @@ char *_strcpy(char *dest, char *src)
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd, ret;
+	int fd, ret;
 
-        if (!filename)
-                return (-1);
+	if (!filename)
+		return (-1);
 
-        fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
-        if (fd == -1)
-                return (fd);
+	if (fd == -1)
+		return (fd);
 
-        if (text_content)
-        {
-                ret = write(fd, text_content, _strlen(text_content));
-                if (ret == -1)
-                {
-                        close(fd);
-                        return (ret);
-                }
-        }
+	if (text_content)
+	{
+		ret = write(fd, text_content, _strlen(text_content));
+		if (ret == -1)
+		{
+			close(fd);
+			return (ret);
+		}
+	}
 
-        close(fd);
-        return (1);
+	close(fd);
+	return (1);
 }
