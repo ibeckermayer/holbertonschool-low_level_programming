71f61405d665f790ed31d135c6b06306bec669cf
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-11 20:23:05 +0000

    betty

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,22 +1,5 @@
 #include "hash_tables.h"
 
-/* /\** */
-/*  * key_index - gives you the index of a key using the hash_djb2 function */
-/*  * @key: the key */
-/*  * @size: size of the array of the hash table */
-/*  * */
-/*  * Return: the index at which the key/value pair */
-/*  * should be stored in the array of the hash table */
-/*  *\/ */
-/* unsigned long int key_index(const unsigned char *key, unsigned long int size) */
-/* { */
-/* 	unsigned long int hash; */
-
-/* 	hash = hash_djb2(key); */
-
-/* 	return (hash % size); */
-/* } */
-
 /**
  * make_hash_node - function to create a new hash node
  * @ht: the hash table it will be added to / updated
