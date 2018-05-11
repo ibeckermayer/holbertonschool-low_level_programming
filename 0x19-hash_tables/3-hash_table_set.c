10ad25db8bc896d9528fd1c55d649fd4603a70ee
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-11 21:29:34 +0000

    allowing value to be NULL

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -61,7 +61,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hn;
 
 	/* error checking */
-	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
