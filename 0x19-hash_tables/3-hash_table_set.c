e1a60b02db2aaa734f6095996a1bff365d1db31a
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-11 20:04:44 +0000

    I think they're compiling with 2, so took it out

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,21 +1,21 @@
 #include "hash_tables.h"
 
-/**
- * key_index - gives you the index of a key using the hash_djb2 function
- * @key: the key
- * @size: size of the array of the hash table
- *
- * Return: the index at which the key/value pair
- * should be stored in the array of the hash table
- */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-	unsigned long int hash;
+/* /\** */
+/*  * key_index - gives you the index of a key using the hash_djb2 function */
+/*  * @key: the key */
+/*  * @size: size of the array of the hash table */
+/*  * */
+/*  * Return: the index at which the key/value pair */
+/*  * should be stored in the array of the hash table */
+/*  *\/ */
+/* unsigned long int key_index(const unsigned char *key, unsigned long int size) */
+/* { */
+/* 	unsigned long int hash; */
 
-	hash = hash_djb2(key);
+/* 	hash = hash_djb2(key); */
 
-	return (hash % size);
-}
+/* 	return (hash % size); */
+/* } */
 
 /**
  * make_hash_node - function to create a new hash node
