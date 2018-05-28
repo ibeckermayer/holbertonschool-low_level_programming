45e15832bb4f9a8815f1c89b4f0d25f73021d81d
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-28 23:05:49 +0000

    adding all and 0 for bubble sort

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble_sort algorithms
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t num_sorted = 0;
+	int tmp;
+
+	while (num_sorted < size - 1)
+	{
+		for (i = 0, num_sorted = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+			else
+				num_sorted++;
+		}
+	}
+
+}
---
+++ 0x1A-sorting_algorithms/README.md
@@ -0,0 +1,20 @@
+# 0x1A. C - Sorting algorithms & Big O
+## 0. Bubble sort
+***
+
+
+
+## 1. Insertion sort
+***
+
+
+
+## 2. Selection sort
+***
+
+
+
+## 3. Quick sort
+***
+Write a function that sorts an array of integers in ascending order using the 
+
