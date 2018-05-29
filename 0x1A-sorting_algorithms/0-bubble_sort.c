a9aa49fd71176a78fab85be4b5db0be3f493243a
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-29 17:18:30 +0000

    took care of case when array is null

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -12,20 +12,22 @@ void bubble_sort(int *array, size_t size)
 	size_t num_sorted = 0;
 	int tmp;
 
-	while (num_sorted < size - 1)
+	if (array != NULL)
 	{
-		for (i = 0, num_sorted = 0; i < size - 1; i++)
+		while (num_sorted < size - 1)
 		{
-			if (array[i] > array[i + 1])
+			for (i = 0, num_sorted = 0; i < size - 1; i++)
 			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				print_array(array, size);
+				if (array[i] > array[i + 1])
+				{
+					tmp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = tmp;
+					print_array(array, size);
+				}
+				else
+					num_sorted++;
 			}
-			else
-				num_sorted++;
 		}
 	}
-
 }
