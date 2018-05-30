5b1dcc3f03479b84629bd18c8d29ad8ff3ba9f02
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-30 04:53:55 +0000

    implemented quick sort from wikipedia code, still no fucking idea how this thing works

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int i, j, pivot, tmp;
+
+	pivot = array[hi];
+	i = lo - 1;
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i = i + 1;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != hi)
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = tmp;
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+
+void quicksort(int *array, int lo, int hi, size_t size)
+{
+	int p;
+
+	if (lo < hi)
+	{
+		p = partition(array, lo, hi, size);
+		quicksort(array, lo, p - 1, size);
+		quicksort(array, p + 1, hi, size);
+	}
+}
+
+/**
+ * quick_sort - implementation of the quick sort algorithm
+ * @array: the array to sort
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	quicksort(array, 0, size - 1, size);
+}
+
