991f060a07c5f071e3e86bc8bd99c2729a2fa842
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-06 18:26:00 +0000

    18-binary_tree_uncle.c

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -6,9 +6,9 @@
  *
  * Return: a pointer to the sibling node.
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_sibling2(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
@@ -23,6 +23,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	return (binary_tree_sibling(node->parent));
+	return (binary_tree_sibling2(node->parent));
 }
 
