91da7300c0d83ceb258a31edccf819c26ea40fcc
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-06 18:27:14 +0000

    18-binary_tree_uncle.c

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_sibling2(binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
