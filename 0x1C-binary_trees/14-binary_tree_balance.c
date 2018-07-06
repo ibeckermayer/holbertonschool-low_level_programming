798bddeb0fd7820d98e1ca2c878c00f92958a0b1
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-06 18:21:02 +0000

    nope, wasn't that

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
-/* /\** */
-/*  * binary_tree_height - measures the height of a binary tree */
-/*  * @tree: pointer to the root node of the tree to traverse */
-/*  * */
-/*  * Return: height of the tree. */
-/*  *\/ */
-/* size_t binary_tree_height(const binary_tree_t *tree) */
-/* { */
-/* 	size_t rheight, lheight; */
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ *
+ * Return: height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t rheight, lheight;
 
-/* 	if (tree == NULL || (tree->right == NULL && tree->left == NULL)) */
-/* 		return (0); */
-/* 	rheight = 1 + binary_tree_height(tree->right); */
-/* 	lheight = 1 + binary_tree_height(tree->left); */
-/* 	return (rheight > lheight ? rheight : lheight); */
-/* } */
+	if (tree == NULL || (tree->right == NULL && tree->left == NULL))
+		return (0);
+	rheight = 1 + binary_tree_height(tree->right);
+	lheight = 1 + binary_tree_height(tree->left);
+	return (rheight > lheight ? rheight : lheight);
+}
 
 /**
  * binary_tree_balance - measures the balance of the tree.
